8569cd815b69f9d070a7ecf9876649a11dd6714b
dmgame <denis.m.pcspace@gmail.com>
2016-10-19 13:13:35 +0300

    add reset from category, add materialyze style, fix bag with search checkbox class

--- js/jquery.filter.js
+++ js/jquery.filter.js
@@ -5,11 +5,14 @@
 		resetFilterClass: 'criterion-all',
 		search: true,
 		searchClass: '.search-input',
+		resetOneCategory: true,
+		allInOneOutput: true,
 	}
 
 	function InputFilter(element, options){
 		this.config = $.extend({},defaults, options);
 		this.element = element;
+		this.elementClass = '.' + $(element).attr('class');
 		this.init();
 	}
 
@@ -26,36 +29,53 @@
 		
 		var self = this;
 
+		$('a.reset-category').addClass('disabled');
 
 		$(this.element).on('click', function() {
 			var elemenObj = self.createObjWriteElemen({})
 			
 			var curentAttr = $(this).attr('data-target');
-			var checkbox = '.checkbox'+'['+'data-target='+'"' + curentAttr + '"' +']'+':checked';
-			
-			
-			
-			$(checkbox).each(function() {
-				elemenObj[curentAttr].push(this.value);	
-			});
+			var checkbox = self.elementClass +'['+'data-target='+'"' + curentAttr + '"' +']'+':checked';
 
+			$(checkbox).each(function() {
+				elemenObj[curentAttr].push(this.value);
+			});
 			if (elemenObj[curentAttr].length) {$(curentAttr).parent().addClass('choose');}
-			else{$(curentAttr).parent().removeClass('choose');}
+				else{$(curentAttr).parent().removeClass('choose');}
 
 			$(curentAttr).text(elemenObj[curentAttr].join(self.config.separator));
-
+			
+			if (elemenObj[curentAttr].length != 0) {$(curentAttr).siblings('a.reset-category').removeClass('disabled')} 
+				else{$(curentAttr).siblings('a.reset-category').addClass('disabled')}
+				
+			// if (self.config.allInOneOutput) {
+			// 	for (var key in elemenObj){
+			// 		elemenObj['all'] += elemenObj[key].join(' ');
+			// 		console.log(elemenObj);
+			// 	}
+			// 	console.log(elemenObj['all']);	
+			// }
 		});
 
 		if (this.config.resetFilterClass) {
 			this.reset(self);
 		}
-
 		if (this.config.search) {
 			var newObj = this.createObjWriteElemen({});
 			this.search(newObj, this.config.searchClass)
 		}
+		if (this.config.resetOneCategory) {
+			this.resetCategory(self);
+		}
 	}
-
+	InputFilter.prototype.resetCategory= function(self){
+		$('.reset-category').click(function(){
+			var category =  '#' + $(this).siblings('div').attr('id');
+			$(category).text('');
+			$(self.elementClass + '['+'data-target='+'"' + category + '"' +']').removeAttr('checked');
+			$(this).addClass('disabled');
+		})
+	}
 	InputFilter.prototype.unicVal = function(value, index, self){
 		return self.indexOf(value) === index;
 	}
@@ -67,6 +87,7 @@
 		for (var i = 0; i < this._attrArray.length; i++) {
 			this._writeElementObj[this._attrArray[i]] = [];
 		}
+		this._writeElementObj['all'] = '';
 		return  this._writeElementObj;
 	}
 	
@@ -79,7 +100,9 @@
 			}
 			$(self.element).removeAttr('checked');
 			self.createObjWriteElemen({})
+			$('a.reset-category').addClass('disabled');
 		});
+
 	}
 
 	InputFilter.prototype.search = function(obj, searchClass) {
@@ -92,11 +115,7 @@
 		            var text = $(inputInLi).val().replace(/\s+/g, ' ').toLowerCase();
 		            return !~text.indexOf(inputUserVal);
 	        	}).hide();
-
 		})
-		
-		
-		
 	}
 
 	$.fn.inputFilter = function(options){
